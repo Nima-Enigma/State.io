07eb69bf354209cac526cf97c915b10c7a0c8283
Nima_Miri <1381.n.m@gmail.com>
2022-01-24 21:58:25 +0330

    primary map

---
+++ CMakeFiles/state.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/state.dir/src/main.c.o"
+  "state"
+  "state.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/state.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
